489899dd4b69bd07c28199b9c02c8f6d942ef079
AlekseyDanchin <aleksey.danchin@gmail.com>
2020-09-21 17:07:48 +0300

    day_06

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return (
-    <div
-      className="App"
-      style={{
-        color: 'red',
-        backgroundColor: "black"
-      }}
-    >Привет из React приложения!</div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.intervalFlag = null;
+
+		this.state = {
+			title: "Текущее время",
+			date: new Date(),
+			counter: 0,
+		};
+	}
+
+	componentDidMount() {
+		this.intervalFlag = setInterval(() => {
+			this.setState((state, props) => ({
+				date: new Date(),
+				counter: state.counter + 1,
+			}));
+		}, 1000);
+	}
+
+	componentDidUpdate() {}
+
+	componentWillUnmount() {
+		clearInterval(this.intervalFlag);
+	}
+
+	render() {
+		return (
+			<div>
+				<p>{this.state.title}</p>
+				<p>{this.state.counter}</p>
+				<p>{this.state.date.toLocaleTimeString()}</p>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,68 +1,14 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-// import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-class Clock extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      title: "Текущее время",
-      date: new Date,
-      counter: 0
-    }
-
-    setInterval(() => {
-      this.setState((state, props) => ({
-        date: new Date,
-        counter: state.counter + 1
-      }))
-    }, 1000)
-  }
-
-  render () {
-    return (
-      <div>
-        <p>{this.state.title}</p>
-        <p>{this.state.counter}</p>
-        <p>{this.state.date.toLocaleTimeString()}</p>
-      </div>
-    )
-  }
-}
-
-// function Clock () {
-//   const [state, setState] = useState({
-//     title: "Текущее время",
-//     date: new Date
-//   })
-
-//   setInterval(() => setState({
-//     title: "Текущее время",
-//     date: new Date
-//   }))
-
-//   return (
-//     <div>
-//       <p>{state.title}</p>
-//       <p>{state.date.toLocaleTimeString()}</p>
-//     </div>
-//   )
-// }
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Clock />,
-  document.querySelector('#root')
-)
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
