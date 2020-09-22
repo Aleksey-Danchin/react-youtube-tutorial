450c2ec70ab28f01fb981024519d1de5e87fcb91
AlekseyDanchin <aleksey.danchin@gmail.com>
2020-09-22 16:26:55 +0300

    day_08

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
 import React from "react";
-import { useState } from "react";
+import Button from "./Button";
 
 function App(props) {
-	const [counter, setCounter] = useState(0);
+	const handlerThirdClick = () => {
+		console.log("3-й клик прошел");
+	};
+
+	const handlerClick = () => {
+		console.log("handlerClick fired");
+	};
+
+	const handlerMouseEnter = () => {
+		console.log("handlerMouseEnter fired");
+	};
 
 	return (
-		<button onClick={() => setCounter(counter + 1)}>
-			На меня нажали {counter} раз.
-		</button>
+		<Button
+			onThirdClick={handlerThirdClick}
+			onClick={handlerClick}
+			onMouseEnter={handlerMouseEnter}
+		>
+			Новый текст для кнопки
+		</Button>
 	);
 }
-
-// class App extends React.Component {
-// 	constructor(props) {
-// 		super(props);
-
-// 		this.state = {
-// 			counter: 0,
-// 		};
-
-// 		this.handlerClick = this.handlerClick.bind(this);
-// 	}
-
-// 	handlerClick() {
-// 		this.setState({ counter: this.state.counter + 1 });
-// 	}
-
-// 	render() {
-// 		return (
-// 			<button
-// 				onClick={() => this.setState({ counter: this.state.counter + 1 })}
-// 			>
-// 				На меня нажали {this.state.counter} раз.
-// 			</button>
-// 		);
-// 	}
-// }
 
 export default App;
