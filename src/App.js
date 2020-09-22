53d7d58b6b385a69afeec566a921cca1564e0d15
AlekseyDanchin <aleksey.danchin@gmail.com>
2020-09-22 16:03:57 +0300

    day_07

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
+function App(props) {
+	const [counter, setCounter] = useState(0);
 
-		this.intervalFlag = null;
-
-		this.state = {
-			title: "Текущее время",
-			date: new Date(),
-			counter: 0,
-		};
-	}
-
-	componentDidMount() {
-		this.intervalFlag = setInterval(() => {
-			this.setState((state, props) => ({
-				date: new Date(),
-				counter: state.counter + 1,
-			}));
-		}, 1000);
-	}
-
-	componentDidUpdate() {}
-
-	componentWillUnmount() {
-		clearInterval(this.intervalFlag);
-	}
-
-	render() {
-		return (
-			<div>
-				<p>{this.state.title}</p>
-				<p>{this.state.counter}</p>
-				<p>{this.state.date.toLocaleTimeString()}</p>
-			</div>
-		);
-	}
+	return (
+		<button onClick={() => setCounter(counter + 1)}>
+			На меня нажали {counter} раз.
+		</button>
+	);
 }
+
+// class App extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.state = {
+// 			counter: 0,
+// 		};
+
+// 		this.handlerClick = this.handlerClick.bind(this);
+// 	}
+
+// 	handlerClick() {
+// 		this.setState({ counter: this.state.counter + 1 });
+// 	}
+
+// 	render() {
+// 		return (
+// 			<button
+// 				onClick={() => this.setState({ counter: this.state.counter + 1 })}
+// 			>
+// 				На меня нажали {this.state.counter} раз.
+// 			</button>
+// 		);
+// 	}
+// }
 
 export default App;
