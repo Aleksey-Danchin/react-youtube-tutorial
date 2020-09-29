99a15516570abd520194489dc28c2aafc3d6c1b6
AlekseyDanchin <aleksey.danchin@gmail.com>
2020-09-29 13:52:17 +0300

    day_13

--- src/App.js
+++ src/App.js
@@ -1,43 +1,56 @@
 import React, { useState } from "react";
 
-import CitiesList from "./CitiesList";
-import City from "./City";
-
 function App() {
-	const [cities, setCities] = useState([
-		{ name: "Москва", description: "Столица России" },
-		{ name: "Зеленоград", description: "Научный спутник Москвы" },
-	]);
+	const [state, setState] = useState({
+		counter: 0,
+		user: {
+			name: "Алексей",
+			surname: "Данчин",
+		},
+	});
 
-	const [currentIndex, setCurrentIndex] = useState(0);
+	console.log(state);
 
-	const handlerChangeCity = (n, description) => {
-		setCities(
-			cities.map((city, index) => {
-				if (index === n) {
-					return {
-						...city,
-						description,
-					};
-				}
-
-				return city;
-			})
-		);
+	const handlerClick = (e) => {
+		setState({
+			...state,
+			counter: state.counter + 1,
+		});
 	};
 
-	const handlerSelectCity = (n) => setCurrentIndex(n);
-
 	return (
-		<div>
-			<City
-				cities={cities}
-				onChangeCity={handlerChangeCity}
-				currentIndex={currentIndex}
-			/>
-			<CitiesList cities={cities} onSelectCity={handlerSelectCity} />
-		</div>
+		<button onClick={handlerClick}>На меня нажали {state.counter} раз.</button>
 	);
 }
+
+// function App() {
+// 	const [counters, setCounters] = useState([0, 0, 0]);
+
+// 	const count = (n) => setCounters(counters.map((x, i) => x + (i === n)));
+
+// 	// const count = (n) => {
+// 	// 	setCounters(
+// 	// 		counters.map((v, i) => {
+// 	// 			if (n === i) {
+// 	// 				return v + 1;
+// 	// 			}
+
+// 	// 			return v;
+// 	// 		})
+// 	// 	);
+// 	// };
+
+// 	return (
+// 		<ul>
+// 			{[0, 1, 2].map((v) => (
+// 				<li key={v}>
+// 					<button onClick={() => count(v)}>
+// 						На меня нажали {counters[v]} раз.
+// 					</button>
+// 				</li>
+// 			))}
+// 		</ul>
+// 	);
+// }
 
 export default App;
