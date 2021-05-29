c0424b04213d6f9a1f9b9c09a96623c93f1214bf
Aleksey-Danchin <aleksey.danchin@gmail.com>
2021-05-29 17:36:36 +0300

    day_24

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from "react";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@/Components";
+
+function MainPage(prop) {
+	const ref = useRef();
+
+	useEffect(() => {
+		console.log(ref.current);
+	}, []);
+
+	return (
+		<Table>
+			<TableHead>
+				<TableRow>
+					<TableCell ref={ref}>id</TableCell>
+					<TableCell>Имя</TableCell>
+					<TableCell>Фамилия</TableCell>
+				</TableRow>
+			</TableHead>
+			<TableBody>
+				<TableRow>
+					<TableCell>1</TableCell>
+					<TableCell onClick={() => console.log("Алексей")}>
+						Алексей
+					</TableCell>
+					<TableCell>Данчин</TableCell>
+				</TableRow>
+
+				<TableRow>
+					<TableCell>2</TableCell>
+					<TableCell>Сергей</TableCell>
+					<TableCell>Воронин</TableCell>
+				</TableRow>
+
+				<TableRow>
+					<TableCell>3</TableCell>
+					<TableCell>Дмитрий</TableCell>
+					<TableCell>Злов</TableCell>
+				</TableRow>
+			</TableBody>
+		</Table>
+	);
+}
+
+export default MainPage;
